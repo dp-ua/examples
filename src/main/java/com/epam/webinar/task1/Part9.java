c680a3d105dd3dc993d89bc950e526cd10b56a3b
Pavel Reshetylo <33344454+dp-ua@users.noreply.github.com>
2019-06-10 23:39:01 +0300

    part9 fix

--- src/main/java/com/epam/webinar/task1/Part9.java
+++ src/main/java/com/epam/webinar/task1/Part9.java
@@ -9,7 +9,7 @@ package com.epam.webinar.task1;
 //        Число n получать как параметр командной строки.
 //        Числа должны быть выведены через пробел, вывод не должен заканчиваться пробельным символом.
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Part9 {
     public static void main(String[] args) {
@@ -24,13 +24,39 @@ public class Part9 {
             System.out.println("Wrong input");
             return;
         }
-        if (n < 1) {
+        if (n < 2) {
             System.out.println("Wrong input");
             return;
         }
-        Part5 part5 = new Part5();
-        int[] numbers = part5.getPrimeNumbers(n);
+        Part9 work = new Part9();
+        int[] numbers = work.getPrimeNumbersLessTheNumber(n);
         System.out.println(Arrays.toString(numbers).replaceAll("[\\[\\],]", ""));
 
     }
+
+    public int[] getPrimeNumbersLessTheNumber(int number) throws IllegalArgumentException {
+        if (number < 2) throw new IllegalArgumentException("Number cant be less than 2");
+        List<Integer> result = new ArrayList<>();
+
+        boolean[] numbers = new boolean[number];
+        Arrays.fill(numbers, true);
+        Part5 part5 = new Part5();
+        for (int i = 1; i < number; i++) {
+            if (numbers[i]) {
+                if (part5.isPrime(i + 1)) {
+                    result.add(i + 1);
+                    for (int j = i; j < number; j++) {
+                        int k = (i + 1) * (j + 1) - 1;
+                        if (k < number) numbers[k] = false;
+                        else break;
+                    }
+                }
+            }
+        }
+        return result.parallelStream().mapToInt(Integer::intValue).toArray();
+
+
+
+    }
+
 }
