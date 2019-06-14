f5dd0f9cdd7af28abcce2a0482cdc2c3e0525ee7
Pavel Reshetylo <33344454+dp-ua@users.noreply.github.com>
2019-06-14 17:14:37 +0300

    #7done

--- src/main/java/com/examples/sweater/config/MvcConfig.java
+++ src/main/java/com/examples/sweater/config/MvcConfig.java
@@ -1,13 +1,25 @@
 package com.examples.sweater.config;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     private final Logger log = Logger.getLogger(MvcConfig.class);
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("file:///" + uploadPath + "/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
--- src/main/java/com/examples/sweater/controller/MainController.java
+++ src/main/java/com/examples/sweater/controller/MainController.java
@@ -4,18 +4,27 @@ import com.examples.sweater.model.Message;
 import com.examples.sweater.model.User;
 import com.examples.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 
 @Controller
 public class MainController {
     @Autowired
     private MessageRepo messageRepo;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting(
@@ -25,7 +34,7 @@ public class MainController {
 
     @GetMapping("/main")
     public String main(@RequestParam(required = false, defaultValue = "") String filter,
-            Model model) {
+                       Model model) {
         Iterable<Message> messages;
 
         if (filter != null && !filter.isEmpty())
@@ -42,16 +51,30 @@ public class MainController {
 
     @PostMapping("/main")
     public String add(
+            @RequestParam("file") MultipartFile file,
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Model model) {
+            @RequestParam String tag, Model model) throws IOException {
         String addText = text.trim();
-        if (addText.length() > 0) {
-            Message message = new Message(addText, tag.trim(), user);
-            messageRepo.save(message);
-        } else {
-            model.addAttribute("error", "Ошибка добавления, пустое сообщение");
+
+        Message message = new Message(addText, tag.trim(), user);
+        if (file != null & !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+            System.out.println("uploadDir: " + uploadDir.getAbsolutePath());
+            if (!uploadDir.exists()) {
+                uploadDir.mkdirs();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath+ "/" + resultFilename));
+
+            message.setFilename(resultFilename);
         }
+
+        messageRepo.save(message);
+
         Iterable<Message> messages = messageRepo.findAll();
         model.addAttribute("messages", messages);
         return "main";
